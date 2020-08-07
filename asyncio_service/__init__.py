705b6ef1c56a9078d003723392b7813808f37261
Kyle Smith <ksmith@smc3.com>
2020-08-07 10:39:16 -0400

    use super() to properly init object

--- asyncio_service/__init__.py
+++ asyncio_service/__init__.py
@@ -5,7 +5,7 @@ from abc import abstractmethod
 
 
 logger = logging.getLogger(__name__)
-__VERSION__ = '1.1'
+__VERSION__ = '1.2-dev'
 __DATE__ = '2020-08-06'
 __MIN_PYTHON__ = (3, 7)
 
@@ -16,6 +16,7 @@ if sys.version_info < __MIN_PYTHON__:
 
 class AsyncioService(object):
     def __init__(self, name=None):
+        super().__init__()
         if name:
             self.name = name
         else:
